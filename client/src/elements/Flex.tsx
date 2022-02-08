b007488f357ca3f0b4a134c6d28de94cbabb789d
jemil-c-137 <jemil1931@gmail.com>
2022-02-08 22:47:43 +0300

    add film delete

--- client/src/elements/Flex.tsx
+++ client/src/elements/Flex.tsx
@@ -1,14 +1,29 @@
 import { styled } from '@mui/system';
 
 interface FlexProps {
+  $wrap?: 'wrap' | 'nowrap' | 'unset';
   $direction?: 'column' | 'row' | 'row-reverse' | 'column-reverse';
   $justify?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
   $align?: 'center' | 'flex-start' | 'flex-end';
+  $colGap?: string;
+  $rowGap?: string;
 }
 
-export const Flex = styled('div')(({ $justify = 'center', $direction = 'row', $align = 'center' }: FlexProps) => ({
-  display: 'flex',
-  justifyContent: $justify,
-  flexDirection: $direction,
-  alignItems: $align,
-}));
+export const Flex = styled('div')(
+  ({
+    $justify = 'center',
+    $direction = 'row',
+    $align = 'center',
+    $wrap = 'nowrap',
+    $colGap = 'normal',
+    $rowGap = 'normal',
+  }: FlexProps) => ({
+    display: 'flex',
+    justifyContent: $justify,
+    flexDirection: $direction,
+    alignItems: $align,
+    flexWrap: $wrap,
+    columnGap: $colGap,
+    rowGap: $rowGap,
+  }),
+);
---
+++ client/src/interfaces/DeleteFilm.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: DeleteFilm
+// ====================================================
+
+export interface DeleteFilm {
+  deleteFilm: boolean;
+}
+
+export interface DeleteFilmVariables {
+  slug: string;
+}
